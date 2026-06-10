066287e2b09a5d2ff6195f4c83a7d4e3b2f3b56d
Varakalasai <suryasticsai.may26@gmail.com>
2026-06-10 21:28:29 +0530

    Update app.py via GrishteSync v0.0.1

---
+++ app.py
@@ -0,0 +1,6 @@
+# Created with GrishteSync
+# https://suryasticsai.github.io/GrishteSync
+# Suryasticsai | suryasticsai@gmail.com
+import streamlit as st
+st.markdown("<h1 style='text-align: center; color: green;'>GrishteSync works!</h1>", unsafe_allow_html=True)
+print('Made with GrishteSync | Suryasticsai | suryasticsai@gmail.com')
